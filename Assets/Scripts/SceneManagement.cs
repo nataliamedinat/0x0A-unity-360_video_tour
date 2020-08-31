aa3ca6910047a7730c3a36c6c7805bf941c7f61a
Natalia <51713603+nataliamedinat@users.noreply.github.com>
2020-08-31 03:02:29 -0500

    Assers: Scenes, scene management

---
+++ Assets/Scripts/SceneManagement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneManagement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public void SceneCantina()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Cantina");
+    }
+
+    public void SceneLivingR()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("360VideoTour");
+    }
+
+    public void SceneCube()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Cube");
+    }
+
+    public void SceneMezz()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Mezzanine");
+    }
+}
